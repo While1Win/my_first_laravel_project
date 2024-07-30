3f390f27a57586616dda345b11c1a28ed717c3ce
while1win <yaman.mouwakeh@le-campus-numerique.fr>
2024-07-30 11:09:08 +0200

    creation de controllers + views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+function () {
+    return view('welcome');
+};
+Route::get('/', function () {
+    return 'Homepage';
+});
+
+Route::get('/product', function () {
+    return 'Liste des produits';
+});
+
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit $id";
+});
+
+Route::get('/cart', function () {
+    return 'Panier';
+});
